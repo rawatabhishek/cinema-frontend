f0c78399022dff94a4e1069c1fd0e163be523df8
Abhishek Rawat <rawat.abhishek023@gmail.com>
2019-09-24 18:55:53 +0530

    added view for the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/layout/header/header.component';
+import { FooterComponent } from './views/layout/footer/footer.component';
+import { LoginComponent } from './views/auth/login/login.component';
+import { ForgotPasswordComponent } from './views/auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './views/auth/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
